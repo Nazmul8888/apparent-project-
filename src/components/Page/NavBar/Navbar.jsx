5df50a554ed024f40855d6ece2329fd3104e40ae
Your Name <you@example.com>
2023-11-24 12:23:16 +0600

    client navbar and banner section update

---
+++ src/components/Page/NavBar/Navbar.jsx
@@ -0,0 +1,48 @@
+import { Link } from 'react-router-dom';
+import logoImg from '../../../assets/logo.jpeg'
+
+const Navbar = () => {
+    const navLinks = <>
+        <li><Link className='font-extrabold text-purple-700 text-1xl' to='/'>CREDENCE</Link></li>
+        <li><Link className='font-extrabold text-green-700 text-1xl' to='/apartment'>APARTMENT</Link></li>
+        <li><Link className='font-extrabold text-pink-700 text-1xl' to='/apartment'>LOGIN</Link></li>
+    </>
+    return (
+        <div className="navbar bg-base-100">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                        {navLinks}
+
+                    </ul>
+                </div>
+                <img className='w-[85px] h-[85px]' src={logoImg} alt="" />
+                <a className="btn btn-ghost font-extrabold text-[#00FFFF] text-2xl">CREDENCE</a>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    {navLinks}
+                </ul>
+            </div>
+
+            <div className="navbar-end">
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost btn-circle">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
+                    </label>
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-green-500 text-white rounded-box w-52">
+                        <li><a>Profile</a></li>
+                        <li><a>DashBoard</a></li>
+                        <li><a>Logout</a></li>
+                    </ul>
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default Navbar;
